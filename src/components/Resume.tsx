842c10e3f42420a76d90e2ad28c83b6c5c6e0f2e
suraj <ridesuraj@gmail.com>
2025-08-25 19:12:19 +0530

    every step done before color centralization

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -38,6 +38,7 @@ export function Resume() {
         </div>
       </div>
       {/* Add extra space at the bottom to extend page height, adjust if 2nd page gets new stuff */}
+      {/* 695 works for 60% a4 */}
       <div className="w-full" style={{ height: '695px' }}></div>
 
 
